ce2d6bf4c22de438e06a15786bae832c41f48a8e
Karsten Løgstrup <61208095+logstrup78@users.noreply.github.com>
2022-12-30 11:30:05 +0100

    Knap til nyt nummer. Endnu ingen mulighed for at den selv klikker feltet af.

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ $('.box').click(function(){
     $('button').click(function() {
         location.reload();
     });
+
+$('.new_number').click(function(){
+    random_number = Math.floor((Math.random(1,90) * 90) + 1) ;
+    alert(random_number)
+})
+
+
+
+
